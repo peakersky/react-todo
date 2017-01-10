cbb6e85312e10e6152e11f1b41b2d9196547927e
David Lee <peakersky@gmail.com>
2017-01-10 22:19:28 +0900

    Add todoApp component

---
+++ app/components/todoApp.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var TodoApp = React.createClass ({
+  render: function () {
+    return (
+      <div>
+        TodoApp.jsx JO!
+      </div>
+    )
+  }
+ })
+
+
+module.exports = TodoApp;
